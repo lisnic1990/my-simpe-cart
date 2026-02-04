0568d1a4b05cb218e05cb9efd92050f4ed9eff98
Alexandr Lisnic <lisnic1990@gmail.com>
2026-02-04 21:44:33 +0200

    added ro-ro

---
+++ upload/admin/language/ro-ro/extension/module/simple_checkout_lite.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Simple Checkout Lite - Admin Language (Romanian)
+ */
+
+// Heading
+$_['heading_title']              = 'Finalizare simplificată (Simple Checkout Lite)';
+
+// Text
+$_['text_extension']             = 'Module';
+$_['text_success']               = 'Setările modulului au fost salvate cu succes!';
+$_['text_edit']                  = 'Editare modul';
+$_['text_enabled']               = 'Activat';
+$_['text_disabled']              = 'Dezactivat';
+$_['text_yes']                   = 'Da';
+$_['text_no']                    = 'Nu';
+$_['text_required']              = 'Obligatoriu';
+$_['text_visible']               = 'Vizibil';
+$_['text_hidden']                = 'Ascuns';
+
+// Entry
+$_['entry_status']               = 'Stare modul';
+$_['entry_guest']                = 'Comandă ca oaspete';
+$_['entry_payment_default']      = 'Metodă de plată implicită';
+$_['entry_shipping_default']     = 'Metodă de livrare implicită';
+
+// Fields
+$_['entry_field_firstname']      = 'Prenume';
+$_['entry_field_lastname']       = 'Nume';
+$_['entry_field_email']          = 'E-mail';
+$_['entry_field_telephone']      = 'Telefon';
+$_['entry_field_address_1']      = 'Adresă';
+$_['entry_field_address_2']      = 'Adresă (suplimentar)';
+$_['entry_field_city']           = 'Oraș';
+$_['entry_field_postcode']       = 'Cod poștal';
+$_['entry_field_country']        = 'Țară';
+$_['entry_field_zone']           = 'Regiune';
+$_['entry_field_company']        = 'Companie';
+
+// Steps
+$_['entry_step_shipping_address'] = 'Adresă de livrare (separată de cea principală)';
+$_['entry_step_shipping_method']  = 'Selectare metodă de livrare';
+$_['entry_step_payment_method']   = 'Selectare metodă de plată';
+$_['entry_step_comment']          = 'Comentariu la comandă';
+
+// Tabs
+$_['tab_general']                = 'Setări generale';
+$_['tab_fields']                 = 'Configurare câmpuri';
+$_['tab_steps']                  = 'Pași finalizare';
+
+// Button
+$_['button_save']                = 'Salvează';
+$_['button_cancel']              = 'Anulează';
+
+// Error
+$_['error_permission']           = 'Nu aveți permisiunea de a edita acest modul!';
---
+++ upload/catalog/language/ro-ro/extension/module/simple_checkout_lite.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Simple Checkout Lite - Catalog Language (Romanian)
+ */
+
+// Heading
+$_['heading_title']         = 'Finalizare comandă';
+
+// Text
+$_['text_checkout']         = 'Finalizare comandă';
+$_['text_customer_info']    = 'Informații client';
+$_['text_shipping_address'] = 'Adresă de livrare';
+$_['text_shipping_method']  = 'Metodă de livrare';
+$_['text_payment_method']   = 'Metodă de plată';
+$_['text_comments']         = 'Comentariu la comandă';
+$_['text_loading']          = 'Se încarcă...';
+$_['text_agree']            = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>Termenii și condițiile</b></a>.';
+$_['text_cart']             = 'Coș de cumpărături';
+$_['text_home']             = 'Acasă';
+
+// Entry
+$_['entry_firstname']       = 'Prenume';
+$_['entry_lastname']        = 'Nume';
+$_['entry_email']           = 'E-Mail';
+$_['entry_telephone']       = 'Telefon';
+$_['entry_company']         = 'Companie';
+$_['entry_address_1']       = 'Adresă';
+$_['entry_address_2']       = 'Adresă (suplimentar)';
+$_['entry_city']            = 'Oraș';
+$_['entry_postcode']        = 'Cod poștal';
+$_['entry_country']         = 'Țară';
+$_['entry_zone']            = 'Regiune / Județ';
+$_['entry_comment']         = 'Comentariu';
+$_['entry_same_address']    = 'Adresa de livrare este aceeași cu adresa de facturare';
+
+// Button
+$_['button_confirm']        = 'Confirmă comanda';
+
+// Error
+$_['error_firstname']       = 'Prenumele trebuie să aibă între 1 și 32 de caractere!';
+$_['error_lastname']        = 'Numele trebuie să aibă între 1 și 32 de caractere!';
+$_['error_email']           = 'Adresa de e-mail nu pare a fi validă!';
+$_['error_telephone']       = 'Telefonul trebuie să aibă între 3 și 32 de caractere!';
+$_['error_address_1']       = 'Adresa trebuie să aibă între 3 și 128 de caractere!';
+$_['error_city']            = 'Orașul trebuie să aibă între 2 și 128 de caractere!';
+$_['error_postcode']        = 'Codul poștal trebuie să aibă între 2 și 10 caractere!';
+$_['error_country']         = 'Vă rugăm să selectați o țară!';
+$_['error_zone']            = 'Vă rugăm să selectați o regiune / județ!';
+$_['error_agree']           = 'Trebuie să citiți și să acceptați Termenii și condițiile!';
+$_['error_shipping']        = 'Atenție: Este necesară o metodă de livrare!';
+$_['error_payment']         = 'Atenție: Este necesară o metodă de plată!';
+$_['error_no_shipping']     = 'Atenție: Nu sunt disponibile opțiuni de livrare!';
+$_['error_address']         = 'Atenție: Adresa este obligatorie!';
+$_['error_guest']           = 'Atenție: Informațiile clientului sunt obligatorii!';
+
+// Totals (fallback)
+$_['text_sub_total']        = 'Subtotal';
+$_['text_total']            = 'Total';
